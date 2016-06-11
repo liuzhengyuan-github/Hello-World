56a5f8a612a08c304cc3244967eb6a428d9919a8
正源 刘 <liuzhengyuan@hotmail.com>
2016-06-11 18:28:18 +0800

    test_1

---
+++ Hello World/Hello World/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(exR2(6));
+            Console.ReadKey();
+        }
+        public static string exR2(int n)
+        {
+            if (n < 0) return "";
+            return exR2(n - 3) + n + exR2(n - 2) + n;
+        }
+    }
+}
